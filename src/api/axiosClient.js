89eef5f1a77ebd7354a0f1ded385cb30e52ddcdd
Nguyễn Minh Nhựt <nhutnm@vntt.com.vn>
2026-06-24 14:27:15 +0700

    Remove source code comments

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Mọi request trong project sẽ dùng chung địa chỉ API này.
 const axiosClient = axios.create({
   baseURL: "https://pokeapi.co/api/v2",
   timeout: 10000,
